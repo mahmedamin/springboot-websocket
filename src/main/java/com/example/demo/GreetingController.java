6dd806a1264992053ae67ce81070445ef84231fa
Ahmed <ahmed.udhi@gmail.com>
2021-07-29 03:16:40 +0500

    WebSocket implementation -> done

--- src/main/java/com/example/demo/GreetingController.java
+++ src/main/java/com/example/demo/GreetingController.java
@@ -1,14 +1,16 @@
 package com.example.demo;
 
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.util.HtmlUtils;
 
 @Controller
 public class GreetingController {
     @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
     public Greeting greet(HelloMessage message) {
-        return new Greeting("Hello," +
+        return new Greeting("Hola, " +
                 HtmlUtils.htmlEscape(message.getName()));
     }
 }
